474e2718c17e49217399a22f0cd70635932bdf55
Simon Allan <simon.allan@cancer.org.uk>
2024-11-11 15:22:57 +0000

    initialise repo

---
+++ test/cloudwatch-dashboards-rds-ecs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CloudwatchDashboardsRdsEcs from '../lib/cloudwatch-dashboards-rds-ecs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cloudwatch-dashboards-rds-ecs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CloudwatchDashboardsRdsEcs.CloudwatchDashboardsRdsEcsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
