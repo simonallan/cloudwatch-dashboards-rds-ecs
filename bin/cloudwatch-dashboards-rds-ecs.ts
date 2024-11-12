eeb9f9046e3eb2cd8d442208557b924b1ee11f49
Simon Allan <simon.allan@cancer.org.uk>
2024-11-12 15:04:56 +0000

    my files are the prettiest

--- bin/cloudwatch-dashboards-rds-ecs.ts
+++ bin/cloudwatch-dashboards-rds-ecs.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CloudwatchDashboardsRdsEcsStack } from '../lib/cloudwatch-dashboards-rds-ecs-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { CloudwatchDashboardsRdsEcsStack } from "../lib/cloudwatch-dashboards-rds-ecs-stack";
 
 const app = new cdk.App();
-new CloudwatchDashboardsRdsEcsStack(app, 'CloudwatchDashboardsRdsEcsStack', {
+new CloudwatchDashboardsRdsEcsStack(app, "CloudwatchDashboardsRdsEcsStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
